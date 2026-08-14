f301a2052b64fbe28dcb110deb98fe65b9601302
jamilcik <jamilciktursunov@gmail.com>
2026-08-14 08:34:29 +0300

    Remove 'Float' print statement from main.py
    Removed print statement for 'Float' data type from output.

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ python_terms = {
     "Dictionary": "Kalit-qiymat (key-value) juftligidan iborat ma'lumot turi.",
     "String": "Matnli ma'lumot turi.",
     "Integer": "Butun sonlarni ifodalovchi ma'lumot turi.",
-    "Float": "O'nlik kasr sonlarni ifodalovchi ma'lumot turi.",
 
 }
 print(python_terms)
@@ -12,5 +11,3 @@ print(python_terms["List"])
 print(python_terms["Dictionary"])
 print(python_terms["String"])
 print(python_terms["Integer"])
-print(python_terms["Float"])
-
